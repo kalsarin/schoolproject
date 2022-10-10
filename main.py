e2f21a290e30b53bd8fdc0c3d5c2a5d30a7f2cda
kalsarin <booklike05@naver.com>
2022-10-11 00:00:28 +0900

    testcommit

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello world")
+#임포트
+
+
+playerdb = [[0 for col in range(6)] for row in range(11)]
+
+#게임규칙 정의 [번호, 수익, 탄소배출량]
+plantation = [0, -10, -15]
+farm = [1, 1, -5]
+finance = [2, 10, 0]
+semiconductor = [3, 20, 5]
+tire = [4, 40, 20]
+thermalpp = [5, 60, 35]
+
+def carbon200down(): #세금면제
+
+def carbon200up(): #톤당 1억
+
+def carbon300up(): #톤당 2억
+
+def carbon400up(): #톤당 4억
+
+#셋업
+print("test")
+
+
+
+#실행부
+
+
+#입출력
