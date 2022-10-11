58fd53677023b2d407cd4e04d36bcd998c47c9e0
kalsarin <booklike05@naver.com>
2022-10-11 19:34:25 +0900

    testcommit

--- venv/hsmain.py
+++ venv/hsmain.py
@@ -2,7 +2,19 @@
 
 
 # 플레이할 사람수 정하기
-playerNum = int(input("플레이할 사람 수는 몇명?")) #인원 수
+while (True):
+    try:
+        playerNum = int(input("게임을 플레이 할 인원은 몇명?")) #인원 수
+
+        if(playerNum < 0):
+            print("다시 입력해 주세요.") #범위가 잘못되었을때 출력
+        else:
+            break
+
+    except ValueError:
+        print("입력값이 숫자가 아닙니다.") #문자열이 들어가 입력값이 숫자가 아닐때 출력
+
+
 totalDB = [[0 for col in range(playerNum + 1)] for row in range(12)]
 
 opDef = [
@@ -46,18 +58,21 @@ def calculate(turn, player):
         tax = 1
     elif (totalCarbon < 400):
         tax = 2
-    else :
+    else:
         tax = 4
 
     if (tax > 0):
         for player in range(playerNum):
             if (totalDB[turn + 1][player + 1] != 0):
-                totalDB[turn + 1][player + 1] -= opDef[totalDB[0][player + 1]][2]
+                totalDB[turn + 1][player + 1] -= (opDef[totalDB[0][player + 1]][2] * tax)
     else:
         print("세금이 없습니다!")
 
-
-
+def printChart():
+    for i in range(1, 11):
+        for j in range(1, 7):
+            print("%3s" % str(totalDB[i][j]), end='')
+        print("")
 
 for player in range(playerNum): #사업 정하기
      totalDB[0][player + 1] = int(input('플레이어 ' + str(player + 1) + ' 번의 사업을 정해주세요 : '))
@@ -67,10 +82,12 @@ for turn in range(turnNum): #턴 진행
     print(str(turn + 1) + " 번째 턴입니다.")
     for player in range(playerNum):
         totalCarbon += initTurn(turn, player)
+        printChart()
+    calculate(turn, player)
+    printChart()
+    print("현재 총 탄소량 : " + str(totalCarbon))
 
-    calculate(turn, player)  #턴 계산
 
-#resultCalculate()
 
 
 
